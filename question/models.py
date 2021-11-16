9831348bc45a38955b97efed4802f37e0a6b68e8
liujie <13698006215@163.com>
2021-11-16 18:08:53 +0800

    完成脚本化处理

--- question/models.py
+++ question/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # 标签实体
 class Tag(models.Model):
     content = models.CharField(verbose_name='标签内容', max_length=50)
-    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+    add_time = models.DateTimeField(verbose_name='添加时间', null=True)
 
     def __str__(self):
         return self.content
@@ -14,12 +14,12 @@ class Tag(models.Model):
 
 # 问题实体
 class Question(models.Model):
-    content = models.CharField(verbose_name='问题内容', max_length=200)
+    content = models.TextField(verbose_name='问题内容')
     username = models.CharField(verbose_name='用户名', max_length=50)
     tag = models.ForeignKey(Tag, verbose_name='标签', on_delete=models.CASCADE, db_index=True, db_constraint=False)
-    avatar_index = models.IntegerField(verbose_name='头像索引', default=random.randint(0,13))
-    add_date = models.DateField(verbose_name='添加日期', default=datetime.now, db_index=True)
-    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+    avatar_index = models.IntegerField(verbose_name='头像索引', null=True)
+    add_date = models.DateField(verbose_name='添加日期', null=True, db_index=True)
+    add_time = models.DateTimeField(verbose_name='添加时间', null=True)
 
     def __str__(self):
         return self.content
@@ -37,9 +37,9 @@ class Answer(models.Model):
     email = models.CharField(verbose_name='邮箱', max_length=50)
     question = models.ForeignKey(Question, verbose_name='问题', on_delete=models.CASCADE, db_index=True, db_constraint=False)
     content = models.TextField(verbose_name='答案')
-    avatar_index = models.IntegerField(verbose_name='头像索引', default=random.randint(0, 13))
-    liked_num = models.IntegerField(verbose_name='点赞数', default=random.randint(100,500))
-    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+    avatar_index = models.IntegerField(verbose_name='头像索引', null=True)
+    liked_num = models.IntegerField(verbose_name='点赞数', null=True)
+    add_time = models.DateTimeField(verbose_name='添加时间', null=True)
 
     def __str__(self):
         return self.content
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,7 +1,9 @@
 # 处理数据
+import re
 import json
 import random
 import time
+import pandas as pd
 from faker import Faker
 from datetime import datetime
 
@@ -22,7 +24,7 @@ def extract_tags(qa_file='D:/BaiduNetdiskDownload/zhidao_qa.json'):
             if tags is not None:
                 tag = tags[0]
                 if tag not in tag_dict:
-                    tag_dict[tag] = len(tag_dict)
+                    tag_dict[tag] = len(tag_dict) + 1
 
             if index % 10000 == 0:
                 print("处理了{}条数据".format(index))
@@ -32,9 +34,10 @@ def extract_tags(qa_file='D:/BaiduNetdiskDownload/zhidao_qa.json'):
 
     with open('./data/tags.txt', 'w', encoding='utf-8') as fw:
         for tag_item in tag_dict.items():
-            single_tag_dict = {'id': tag_item[1], 'content': tag_item[0]}
-            print(single_tag_dict)
-            fw.write(json.dumps(single_tag_dict) + '\n')
+            content = tag_item[0]
+            id_ = tag_item[1]
+            add_time = datetime.strftime(datetime.fromtimestamp(time.time()), '%Y-%m-%d %H:%M:%S')
+            fw.write('{}\t{}\t{}\n'.format(id_, content, add_time))
 
 
 # 抽取问题和答案
@@ -43,9 +46,11 @@ def extract_question_answers(qa_file='D:/BaiduNetdiskDownload/zhidao_qa.json'):
         tag_dict = json.loads(fr.read())
 
     file_count = 0
-    question_f = open('./data/question_{}'.format(file_count), 'w', encoding='utf-8')
-    answer_f = open('./data/answer_{}'.format(file_count), 'w', encoding='utf-8')
     answer_id = 1
+    q_id_list, q_content_list, q_add_date_list, q_add_time_list, q_tag_id_list, q_username_list, q_avatar_index_list \
+        = [], [], [], [], [], [], []
+    a_id_list, a_username_list, a_email_list, a_content_list, aq_id_list, a_add_time_list, a_avatar_index_list, \
+    a_liked_num_list = [], [], [], [], [], [], [], []
 
     with open(qa_file, 'r', encoding='utf-8') as fr:
         for index, line in enumerate(fr):
@@ -57,39 +62,82 @@ def extract_question_answers(qa_file='D:/BaiduNetdiskDownload/zhidao_qa.json'):
             answers = json_line.get('answers', None)
 
             if question is not None and tags is not None and answers is not None:
-                question_time = time.time() - 24 * 3600 - random.randint(1, 5) * 3600 - random.randint(1, 60)
-                question_dict = {
-                    'id': question_id,
-                    'content': question,
-                    'tag_id': tag_dict[tags[0]],
-                    'username': fake.name(),
-                    'avatar_index': random.randint(0, 13),
-                    'add_date': datetime.strftime(datetime.fromtimestamp(question_time), '%Y-%m-%d'),
-                    'add_time': datetime.strftime(datetime.fromtimestamp(question_time), '%Y-%m-%d %H:%M:%S'),
-                }
+                question_time = time.time() + (question_id//100000) * 3600 - random.randint(1, 5) * 3600 - random.randint(1, 60)
+
+                id_ = question_id
+                content = re.sub('\t', '', question)
+                add_date = datetime.strftime(datetime.fromtimestamp(question_time), '%Y-%m-%d')
+                add_time = datetime.strftime(datetime.fromtimestamp(question_time), '%Y-%m-%d %H:%M:%S')
+                tag_id = tag_dict[tags[0]]
+                username = fake.name()
+                question_avatar_index = random.randint(0, 13)
+
+                q_id_list.append(id_)
+                q_content_list.append(content)
+                q_add_date_list.append(add_date)
+                q_add_time_list.append(add_time)
+                q_tag_id_list.append(tag_id)
+                q_username_list.append(username)
+                q_avatar_index_list.append(question_avatar_index)
+
                 # 写入问题
-                question_f.write(json.dumps(question_dict) + '\n')
+                # question_f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(id_, content, add_date, add_time, tag_id, username, question_avatar_index))
+
                 # 写入答案
                 for answer in answers:
                     answer_time = question_time + random.randint(1, 5) * 3600 + random.randint(1, 60)
-                    answer_dict = {
-                        'id': answer_id,
-                        'username': fake.name(),
-                        'email': fake.email(),
-                        'content': answer,
-                        'question_id': question_id,
-                        'add_time': datetime.strftime(datetime.fromtimestamp(answer_time), '%Y-%m-%d %H:%M:%S'),
-                        'avatar_index': random.randint(0, 13),
-                        'liked_num': random.randint(500, 2000)
-                    }
-                    answer_f.write(json.dumps(answer_dict) + '\n')
+
+                    _id = answer_id
+                    username = fake.name()
+                    email = fake.email()
+                    content = answer
+                    add_time = datetime.strftime(datetime.fromtimestamp(answer_time), '%Y-%m-%d %H:%M:%S')
+                    answer_avatar_index = random.randint(0, 13)
+                    liked_num = random.randint(500, 2000)
+
+                    a_id_list.append(_id)
+                    a_username_list.append(username)
+                    a_email_list.append(email)
+                    a_content_list.append(content)
+                    a_add_time_list.append(add_time)
+                    a_avatar_index_list.append(answer_avatar_index)
+                    a_liked_num_list.append(liked_num)
+                    aq_id_list.append(question_id)
+
+                    # answer_f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(_id, username, email, content, question_id, add_time, answer_avatar_index, liked_num))
                     answer_id += 1
 
             if question_id % 10000 == 0:
+                question_df = pd.DataFrame({
+                    'id': q_id_list,
+                    'content': q_content_list,
+                    'add_date': q_add_date_list,
+                    'add_time': q_add_time_list,
+                    'tag_id': q_tag_id_list,
+                    'username': q_username_list,
+                    'question_avatar_index': q_avatar_index_list
+                })
+                question_df.to_csv('./data/question_{}.csv'.format(file_count), index=False)
+
+                answer_df = pd.DataFrame({
+                    'id': a_id_list,
+                    'username': a_username_list,
+                    'email': a_email_list,
+                    'content': a_content_list,
+                    'question_id': aq_id_list,
+                    'add_time': a_add_time_list,
+                    'answer_avatar_index': a_avatar_index_list,
+                    'liked_num': a_liked_num_list,
+                })
+                answer_df.to_csv('./data/answer_{}.csv'.format(file_count), index=False)
                 print("完成第{}个数据文件操作".format(file_count))
+
+                q_id_list, q_content_list, q_add_date_list, q_add_time_list, q_tag_id_list, q_username_list, q_avatar_index_list \
+                    = [], [], [], [], [], [], []
+                a_id_list, a_username_list, a_email_list, a_content_list, aq_id_list, a_add_time_list, a_avatar_index_list, \
+                a_liked_num_list = [], [], [], [], [], [], [], []
+
                 file_count += 1
-                question_f.close()
-                answer_f.close()
                 break
                 # question_f = open('./data/question_{}'.format(file_count), 'w', encoding='utf-8')
                 # answer_f = open('./data/answer_{}'.format(file_count), 'w', encoding='utf-8')
